9ad2e60852b3b9c8859a2303360f0c1f3a40041f
Nemo D'acremont <ndacremont@enseirb-matmeca.fr>
2024-02-08 15:55:54 +0100

    patch resetnmaster

--- dwm.c
+++ dwm.c
@@ -190,6 +190,7 @@ static void pop(Client *c);
 static void propertynotify(XEvent *e);
 static void quit(const Arg *arg);
 static Monitor *recttomon(int x, int y, int w, int h);
+static void resetnmaster(const Arg *arg);
 static void resize(Client *c, int x, int y, int w, int h, int interact);
 static void resizeclient(Client *c, int x, int y, int w, int h);
 static void resizemouse(const Arg *arg);
@@ -1287,6 +1288,13 @@ recttomon(int x, int y, int w, int h)
 			r = m;
 		}
 	return r;
+}
+
+void
+resetnmaster(const Arg *arg)
+{
+	selmon->nmaster = 1;
+	arrange(selmon);
 }
 
 void
